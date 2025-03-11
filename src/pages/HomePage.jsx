10767988a971e07a26534006988f4258a9c702e0
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-03-11 18:05:01 +0100

    update HomePage title and subtitle

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,8 @@ export default function HomePage() {
 
     return (
         <>
-            <h1 className="text-primary">Bool Books</h1>
-            <h2><i>The nerdest book community</i></h2>
+            <h1 className="text-primary">Bool Movies</h1>
+            <h2><i>The nerdest movies community</i></h2>
 
             <div className="row row-cols-3 mt-4">
                 <MovieCard />
